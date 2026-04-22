da06d50d7809a7dd1033beb1c05b97f3d057e70b
Vinit Kumar <mail@vinitkumar.me>
2026-04-22 09:42:01 +0530

    chore: update theme build tooling (#83)

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite';
-import { readFileSync } from 'fs';
-import path from 'path';
-import rtlcss from 'rtlcss';
-import postcssRTLCSS from 'postcss-rtlcss';
-
-const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'css/main.scss',
-        rtl: 'css/main.scss'
+        main: 'css/main.scss'
       },
       output: {
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
+          if (assetInfo.name?.endsWith('.css')) {
             return 'css/[name].min[extname]';
           }
           return 'assets/[name][extname]';
@@ -23,12 +16,5 @@ export default defineConfig({
       }
     },
     cssMinify: true,
-  },
-  css: {
-    postcss: {
-      plugins: [
-        postcssRTLCSS()
-      ]
-    }
   }
 });
